ab3aaa7c147e727daa653201669ea4389a81a901
Levi Paradis <lparadis@vendasta.com>
2016-06-10 15:30:54 -0600

    get methods

--- interactive.php
+++ interactive.php
@@ -7,16 +7,6 @@ $password = $_GET["password"];
 $filename = $_GET["filename"];
 $caption = $_GET["caption"];
 
-/*
-// $username = 'leviparadis123';
-// $password = 'leviiscool1';
-*/
-// Set the path to the file that you wish to post.
-// This must be jpeg format and it must be a perfect square
-$filename = 'square.jpg';
-
-// Set the caption for the photo
-$caption = 'Test';
 ProcessPost($username,$password,$filename,$caption);
 
 
